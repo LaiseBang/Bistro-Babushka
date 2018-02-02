a71aa097e14e904d7d1b386047e95cc398c3cfe9
Laise Bang Henriksen <31646151+LaiseBang@users.noreply.github.com>
2018-02-02 12:46:31 +0100

    Alle filer

---
+++ restaurant_javascript.js
@@ -0,0 +1,65 @@
+	//de skarpe parenterser i variablen viser at det er et array
+	let retter = [];
+
+	document.addEventListener("DOMContentLoaded", hentJson);
+
+	async function hentJson() {
+		console.log("async function");
+		let jsonData = await fetch("menu.json");
+		retter = await jsonData.json();
+		vis(retter, "Menu");
+		lavFilter();
+	}
+
+	function vis(retter, overskrift) {
+		console.log("vis retter");
+		let temp = document.querySelector("[data-rettemplate]");
+		let dest = document.querySelector("[data-liste]");
+		dest.innerHTML = "";
+		document.querySelector("[data-overskrift]").textContent = overskrift;
+		//loop
+		retter.forEach(ret => {
+			let klon = temp.cloneNode(true).content;
+			klon.querySelector("[data-navn]").textContent = ret.navn;
+
+			/*kan bruges hvis man har brugt src"" og alt="" i im tagget*/
+			klon.querySelector("[data-billede]").src = "imgs/small/" + ret.billede + "-sm.jpg";
+			klon.querySelector("[data-billede]").alt = "Billede af " + ret.navn;
+			klon.querySelector("[data-kortbeskrivelse]").textContent = ret.kortbeskrivelse;
+			klon.querySelector("[data-pris]").textContent = ret.pris + ",- kr";
+			dest.appendChild(klon);
+
+			/*Alternativ måde at vælge billederne på */
+			/*	klon.querySelector(".billede").setAttribute("src", "imgs/small/" + ret.billede + "-sm.jpg");
+				klon.querySelector(".billede").setAttribute("alt", "Billede af " + ret.navn);*/
+		});
+	}
+
+	function lavFilter() {
+		console.log("filter");
+		let forretter = retter.filter(ret => ret.kategori == "forretter");
+		let hovedretter = retter.filter(ret => ret.kategori == "hovedretter");
+		let sideorders = retter.filter(ret => ret.kategori == "sideorders");
+		let desserter = retter.filter(ret => ret.kategori == "desserter");
+		let drikkevarer = retter.filter(ret => ret.kategori == "drikkevarer");
+
+		//sorter retter efter kategori
+		document.querySelector("#filter-alle").addEventListener("click", () => {
+			vis(retter, "Menu");
+		});
+		document.querySelector("#filter-forretter").addEventListener("click", () => {
+			vis(forretter, "Forretter");
+		});
+		document.querySelector("#filter-hovedretter").addEventListener("click", () => {
+			vis(hovedretter, "Hovedretter");
+		});
+		document.querySelector("#filter-sideorders").addEventListener("click", () => {
+			vis(sideorders, "Sideorders");
+		});
+		document.querySelector("#filter-desserter").addEventListener("click", () => {
+			vis(desserter, "Desserter");
+		});
+		document.querySelector("#filter-drikkevarer	").addEventListener("click", () => {
+			vis(drikkevarer, "Drikkevarer");
+		});
+	}
